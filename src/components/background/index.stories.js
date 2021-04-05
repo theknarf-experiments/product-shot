5ab98175eacad5c2ca841b02295a1178b080fe41
Frank Lyder Bredland <fl@nyggen.com>
2021-04-05 11:42:06 +0200

    Setup storybook

---
+++ src/components/background/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Component from '.';
+
+export default {
+	title: 'components/background',
+};
+
+export const Default = ({ ...args }) => <Component {...args} />;
---
+++ src/components/browser/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Component from '.';
+
+export default {
+	title: 'components/browser'
+};
+
+export const Default = ({ ...args }) => <Component {...args} />;
